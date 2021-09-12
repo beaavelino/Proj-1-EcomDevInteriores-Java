df9cbe2cc37342373854c72a125407e8ef385f19
dbonach <deivitlopes@gmail.com>
2021-09-12 00:54:47 -0300

    Planejamento

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,78 @@
+import java.util.Scanner;
 
 public class Main {
 	public static void main(String[] args) {
 		
 		System.out.println("Hello world");
+		
+		/*
+		 * 
+		 * Boolean comprar = true;
+		 * 
+		 * while(comprar) {
+		 * 
+		 * 	  Apresentacoes.inicio();
+		 * 
+		 * 	  if (Apresentacoes.pergunta()) {
+		 * 	 
+		 *   	  Apresentacoes.desenho();
+		 *   
+		 *   	  Menu.mostraMenu(); // Dentro do menu faz a compra e finaliza compra
+		 *   
+		 *   	  FechamentoCompra.escolhePagamento() // Trabalha com a classe TipoPagamento 
+		 *   
+		 *   	  FechamentoCompra.imprimeNota() 
+		 *   
+		 *   	  comprar = FechamentoCompra.continuarCompra() // Retorna boolean
+		 * 
+		 *   	  Apresentacoes.fim();
+		 *   
+		
+		 * 	  } else {
+		 *   	  Apresentacoes.fim();
+		 * 	  }
+		 * 
+		 * }
+		 * 
+		 * 
+		 */
+		
+		
+		/* @Deivit
+		 * Classe Estoque
+		 *   List<Produto>
+		 *   
+		 *   metodo para retirar produto do estoque
+		 *   metodo para mostrar todos os items
+		 *   
+		 * 
+		 * @Ivan @Gustavo
+		 * Classe Produto
+		 * 	 nome
+		 *   codigo
+		 *   quantidadeEmEstoque
+		 *   quantiadeCarrinho
+		 *   valor
+		 *   
+		 *   toString
+		 *   
+		 * @Bea @Fabio
+		 * Classe Carrinho
+		 * 	 lista com produtos
+		 * 
+		 * 	 metodo para adicionar item ao carrinho
+		 *   
+		 * @Bea @Fabio
+		 * Classe FechamentoCompra
+		 * 	 tipoPagamento
+		 * 
+		 * @Alexa
+		 * Classe Apresentacoes
+		 * 	
+		 * @Ivan @Gustavo
+		 * Classe Menu
+		 * 
+		 * 
+		 */
 	}
 }
